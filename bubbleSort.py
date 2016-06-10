31ea84802279ec09e4e247a36746f03cb2713e59
Ryan Orozco <ryan.orozco@gmail.com>
2016-06-10 13:58:48 -0700

    just practicing bubble sort

---
+++ bubbleSort.py
@@ -0,0 +1,18 @@
+import random
+
+my_randoms = random.sample(xrange(100001), 100)
+
+
+def bubbleSort(unsorted_list):
+    length = len(unsorted_list) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(length):
+            if unsorted_list[i] > unsorted_list[i+1]:
+                sorted = False
+                unsorted_list[i], unsorted_list[i+1] = unsorted_list[i+1], unsorted_list[i]
+
+bubbleSort(my_randoms)
+print my_randoms
